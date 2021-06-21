f93520cee49bdf99ed1a65aaac97db23dda98fc3
Ari Birnbaum <ari@ceiphr.com>
2021-06-21 01:35:28 -0400

    Added django post. Finished prism config.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,7 @@
 // import "typeface-merriweather"
 import './src/styles/global.css';
 import './src/styles/prism.css';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+import 'prismjs/plugins/command-line/prism-command-line.css';
 // Highlighting for code blocks
 // import "prismjs/themes/prism.css"
--- gatsby-config.js
+++ gatsby-config.js
@@ -53,7 +53,17 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`
             }
           },
-          `gatsby-remark-prismjs`,
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              inlineCodeMarker: "±",
+              prompt: {
+                user: "ari",
+                host: "localhost",
+                global: true
+              }
+            }
+          },
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`
         ]
