e389688256c04bdf8612342b9a598e46d7142171
Ari Birnbaum <ari@ceiphr.com>
2021-06-20 23:42:00 -0400

    Implemented giscus.

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -34,11 +34,14 @@ const BlogPostTemplate: React.FC<PageProps<DataProps>> = ({ data, location }) =>
   React.useEffect(() => {
     const scriptEl = document.createElement('script')
     scriptEl.async = true
-    scriptEl.src = 'https://utteranc.es/client.js'
-    scriptEl.setAttribute('repo', 'ceiphr/ceiphr.com-comments')
-    scriptEl.setAttribute('issue-term', 'title')
-    scriptEl.setAttribute('id', 'utterances')
-    scriptEl.setAttribute('theme', 'preferred-color-scheme')
+    scriptEl.src = 'https://giscus.app/client.js'
+    scriptEl.setAttribute('data-repo', 'ceiphr/ceiphr.com-comments')
+    scriptEl.setAttribute('data-repo-id', 'MDEwOlJlcG9zaXRvcnkzNzg3NDY2Mzg=')
+    scriptEl.setAttribute('data-category', 'Announcements')
+    scriptEl.setAttribute('data-category-id', 'MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMzMDQzMTY5')
+    scriptEl.setAttribute('data-mapping', 'title')
+    scriptEl.setAttribute('data-reactions-enabled', '1')
+    scriptEl.setAttribute('data-theme', 'preferred_color_scheme')
     scriptEl.setAttribute('crossorigin', 'anonymous')
     if (commentBox && commentBox.current) {
       commentBox.current.appendChild(scriptEl)
