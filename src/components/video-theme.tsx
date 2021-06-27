f5d477738e8fecf613c77c845409ff7921f1d6eb
Ari Birnbaum <ari@ceiphr.com>
2021-06-27 01:03:29 -0400

    Fixing missing object warning/error for real. Added transition effect on load.

--- src/components/video-theme.tsx
+++ src/components/video-theme.tsx
@@ -1,54 +1,37 @@
 import * as React from "react"
-import { isIOS, isSafari } from "react-device-detect"
 
 import Background from "../../static/videos/dark.webm"
 import BackgroundDark from "../../static/videos/light.webm"
 
-function VideoTheme() {
-  const [safari, setSafari] = React.useState()
-
-  React.useEffect(() => {
-    // @ts-ignore
-    setSafari(isSafari)
-  }, [setSafari])
-
-  const [IOS, setIOS] = React.useState()
-
-  React.useEffect(() => {
-    // @ts-ignore
-    setIOS(isIOS)
-  }, [setIOS])
-
-  const [mQuery, setMQuery] = React.useState<any>({
-    matches: window.matchMedia("(prefers-color-scheme: dark)").matches ? BackgroundDark : Background
-  })
+const VideoTheme = () => {
+  const [mQuery, setMQuery] = React.useState<any>(Background)
 
   const videoRef = React.useRef() as any
   const previousUrl = React.useRef(mQuery)
 
   React.useEffect(() => {
-    let mediaQuery = window.matchMedia("(prefers-color-scheme: dark)")
-    mediaQuery.addEventListener("change", (e) => {
-      e.matches ? setMQuery(BackgroundDark) : setMQuery(Background)
-    })
-    setMQuery(mediaQuery.matches ? BackgroundDark : Background)
+    if (typeof window !== "undefined") {
+      let mediaQuery = window.matchMedia("(prefers-color-scheme: dark)")
+      mediaQuery.addEventListener("change", (e) => {
+        e.matches ? setMQuery(BackgroundDark) : setMQuery(Background)
+      })
+      setMQuery(mediaQuery.matches ? BackgroundDark : Background)
 
-    if (previousUrl.current === mQuery)
-      return
-    if (typeof videoRef.current !== "undefined" && videoRef.current)
-      videoRef.current.load()
+      if (previousUrl.current === mQuery)
+        return
+      if (typeof videoRef.current !== "undefined" && videoRef.current)
+        videoRef.current.load()
 
-    previousUrl.current = mQuery
+      previousUrl.current = mQuery
+    }
   }, [mQuery])
-
-  if (safari || IOS) return (<></>)
 
   return (
     <>
       <label htmlFor="background" className="sr-only">
         Background Video
       </label>
-      <video id="background" ref={videoRef} loop autoPlay muted className="object-cover h-full w-full"
+      <video id="background" ref={videoRef} loop autoPlay muted className="object-cover h-full w-full fade-in"
              src={mQuery} />
     </>
   )
