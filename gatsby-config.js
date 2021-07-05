e6140f3eb00e19f695d7b3c38d67d2c8a15e937e
Ari Birnbaum <ari@ceiphr.com>
2021-07-04 20:20:50 -0400

    Updated video and image backgrounds for topo v2.

--- gatsby-config.js
+++ gatsby-config.js
@@ -172,7 +172,7 @@ module.exports = {
         theme_color: `#111827`,
         display: `minimal-ui`,
         include_favicon: false,
-        icon: `src/images/ari.png`, // This path is relative to the root of the site.
+        icon: `src/images/ari.jpg`, // This path is relative to the root of the site.
         icon_options: {
           // For all the options available,
           // please see the section "Additional Resources" below.
